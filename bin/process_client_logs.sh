3cc8bb635c4018e1b730c46c06cce743b9234f7b
ChenfeiP <peng0368@morris.umn.edu>
2024-09-19 17:39:17 -0500

    Pass all tests for process_client-logs.sh

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Change to the specified directory
-cd "$1" || { echo "Directory not found: $1"; exit 1; }
+cd "$1"
 
-# Process the log files
-cat * | awk '
-    # Pattern for invalid users
-    /Failed password for invalid user/ {
-        # Example: Aug 14 06:00:36 computer_name sshd[26795]: Failed password for invalid user admin from 218.2.129.13 port 59638 ssh2
-        # Extract fields: $1 (Month), $2 (Day), $3 (Time), $9 (Username), $11 (IP)
-        split($3, time, ":");  # Remove minutes and seconds from time
-        print $1, $2, time[1], $9, $11;
-    }
-    # Pattern for valid users
-    /Failed password for / && !/invalid user/ {
-        # Example: Aug 14 06:00:41 computer_name sshd[26798]: Failed password for root from 218.2.129.13 port 62901 ssh2
-        # Extract fields: $1 (Month), $2 (Day), $3 (Time), $9 (Username), $11 (IP)
-        split($3, time, ":");  # Remove minutes and seconds from time
-        print $1, $2, time[1], $9, $11;
-    }
-' > failed_login_data.txt
+find . -type f -exec file --mime {} + | grep 'charset=binary' -v | cut -d: -f1 | \
+xargs cat | iconv -f UTF-8 -t UTF-8 -c | awk '
 
-echo "Processing complete. Output saved to failed_login_data.txt"
+# Case 1: Handling invalid user log entries
+/Failed password for invalid user/ {
+    date = $1 " " $2
+    time = substr($3, 1, 2)
+    user = $(NF-5)
+    ip = $(NF-3)
+    print date, time, user, ip
+}
+
+# Case 2: Handling valid user log entries (excluding invalid users)
+/Failed password for/ && !/invalid user/ {
+    date = $1 " " $2
+    time = substr($3, 1, 2)
+    user = $(NF-5)
+    ip = $(NF-3)
+    print date, time, user, ip
+}'  > failed_login_data.txt
+
