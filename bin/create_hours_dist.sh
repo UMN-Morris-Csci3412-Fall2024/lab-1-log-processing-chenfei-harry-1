3858757a0d4b70be073b7ad53faef78bae574e72
ChenfeiP <peng0368@morris.umn.edu>
2024-09-22 17:10:43 -0500

    pass all tests for the third

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+input_dir="$1"
+
+if ! cd "$input_dir"; then
+  echo "Error: Unable to access directory $input_dir"
+  exit 1
+fi
+
+output_file="hours_dist.html"
+header_file="$current_dir/html_components/hours_dist_header.html"
+footer_file="$current_dir/html_components/hours_dist_footer.html"
+
+if [ -f "$header_file" ]; then
+  cat "$header_file" > "$output_file"
+else
+  echo "Header file not found (current directory: $current_dir): $header_file"
+  exit 1
+fi
+
+temp_file=$(mktemp)
+
+find . -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+  failed_login_file="$dir/failed_login_data.txt"
+  if [ -f "$failed_login_file" ]; then
+    awk '{print $3}' "$failed_login_file" >> "$temp_file"
+  fi
+done
+
+if [ -s "$temp_file" ]; then
+  sort "$temp_file" | uniq -c | while read -r occurrences hour; do
+    printf "data.addRow([\x27%s\x27, %d]);\n" "$hour" "$occurrences" >> "$output_file"
+  done
+else
+  echo "No hours data found."
+  exit 1
+fi
+
+if [ -f "$footer_file" ]; then
+  cat "$footer_file" >> "$output_file"
+else
+  echo "Footer file not found: $footer_file"
+  exit 1
+fi
+
+rm -f "$temp_file"
